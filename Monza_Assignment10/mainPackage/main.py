f76b88ef007a1b7f0798f450d1525075b8cd0a41
slocumjt <slocumjt@VDI-LCB-IC-010>
2025-04-08 13:27:25 -0400

    Added code to connect to API to main

--- Monza_Assignment10/mainPackage/main.py
+++ Monza_Assignment10/mainPackage/main.py
@@ -1,2 +1,18 @@
 # main.py
+import json
+import requests
+
+response = requests.get('https://wft-geo-db.p.rapidapi.com/v1/geo/cities?countryIds=US&limit=10&sort=-population', 
+                        headers={
+                            'X-RapidAPI-Key': '4cc88cdf7cmshf7b29940764088ap104c7ajsnb09745931509',
+                            'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'})
+
+json_string = response.content
+
+parsed_json = json.loads(json_string) # Now we have a python dictionary
+
+total = int(parsed_json['metadata']['totalCount']) # The number of cities that were returned
+
+for city in parsed_json['data']:
+    print(city)
 
